de719071dcee978320a30abae8a66af627155367
Anamaria Camarasan <anamaria.finn0@gmail.com>
2023-01-13 13:01:52 +0200

    fixing cors issues

--- server/index.js
+++ server/index.js
@@ -4,13 +4,17 @@ const server = require("http").createServer(app);
 const socketServer = require("./socket.js");
 
 const cors = require("cors");
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://birdnest-client-0et7.onrender.com/",
+  })
+);
 
 require("dotenv").config();
 const port = process.env.PORT || 5001;
 
 socketServer(server);
 
-server.listen(port,  () => {
+server.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`);
 });
